2f8c1a5b063af85adec043e470710c2d4ee201eb
Matthew Nguyen <matthewnguyen.667@gmail.com>
2020-07-06 19:24:39 -0700

    Fixed output of readcounts

--- src/lib/bamrc/BasicStat.cpp
+++ src/lib/bamrc/BasicStat.cpp
@@ -158,7 +158,7 @@ std::ostream& operator<<(std::ostream& s, const BasicStat& stat) {
         s << 0 << ":";
         s << 0.0 << ":";
         s << 0.0 << ":";
-        s << 0.0;
+        s << 0.0 << ":";
         s << 0.0;
     }
     s.flags(current_flags); //save previous format flags
